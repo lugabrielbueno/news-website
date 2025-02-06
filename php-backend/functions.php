80bcbe10addffa0591f1ed08d2cf6a9be5ad666e
Lucas G. Bueno <lucasgbueno@proton.me>
2025-02-06 13:36:01 -0300

    deploy production

--- php-backend/functions.php
+++ php-backend/functions.php
@@ -10,18 +10,15 @@ function loadEnv($file)
   $lines = file($file, FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
 
   foreach ($lines as $line) {
-    // Ignore lines that are comments
     if (strpos(trim($line), '#') === 0) {
       continue;
     }
 
-    // Split the line into key-value pairs
     $parts = explode('=', $line, 2);
     if (count($parts) === 2) {
       $key = trim($parts[0]);
       $value = trim($parts[1]);
 
-      // Set the environment variable
       putenv("$key=$value");
     }
   }
